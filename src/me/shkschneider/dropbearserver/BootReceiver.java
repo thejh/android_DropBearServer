fe08f3818aefddbcf80c545b5a073ded719d878c
Alan SCHNEIDER <shk.schneider@gmail.com>
2012-04-27 15:28:30 +0200

    * Implements notification at boot

--- src/me/shkschneider/dropbearserver/BootReceiver.java
+++ src/me/shkschneider/dropbearserver/BootReceiver.java
@@ -6,6 +6,9 @@ package me.shkschneider.dropbearserver;
 import me.shkschneider.dropbearserver.Tasks.ServerStarter;
 import me.shkschneider.dropbearserver.Utils.ServerUtils;
 import me.shkschneider.dropbearserver.Utils.ShellUtils;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -16,8 +19,8 @@ public class BootReceiver extends BroadcastReceiver {
 	private static final String TAG = "DropBearServer";
 
 	@Override
-	public void onReceive(Context context, Intent intent) {
-		if (intent.getAction().equals(android.content.Intent.ACTION_BOOT_COMPLETED)) {
+	public void onReceive(Context context, Intent i) {
+		if (i.getAction().equals(android.content.Intent.ACTION_BOOT_COMPLETED)) {
 			Log.d(TAG, "BootReceiver: onReceive(ACTION_BOOT_COMPLETED)");
 
 			Boolean hasPid = (ServerUtils.getServerPidFromFile(context) > 0 ? true : false);
@@ -31,9 +34,30 @@ public class BootReceiver extends BroadcastReceiver {
 			if (startAtBoot) {
 				if (!onlyIfRunningbefore || (onlyIfRunningbefore && hasPid)) {
 					Log.i(TAG, "BootReceiver: DropBear Server starting at boot");
+					
 					// ServerStarter
 					ServerStarter serverStarter = new ServerStarter(null, null);
 					serverStarter.execute();
+					
+					// notification
+					if (SettingsHelper.getInstance(context).getNotification() == true) {
+						String infos = "ssh ";
+						if (SettingsHelper.getInstance(context).getCredentialsLogin() == true) {
+							infos = infos.concat("root@");
+						}
+						infos = infos.concat(ServerUtils.getLocalIpAddress());
+						if (SettingsHelper.getInstance(context).getListeningPort() != SettingsHelper.LISTENING_PORT_DEFAULT) {
+							infos = infos.concat(" -p " + SettingsHelper.getInstance(context).getListeningPort());
+						}
+						
+						NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+						Notification notification = new Notification(R.drawable.ic_launcher, "DropBear Server is running", System.currentTimeMillis());
+						Intent intent = new Intent(context, MainActivity.class);
+						PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+						notification.setLatestEventInfo(context, "DropBear Server", infos, pendingIntent);
+						notification.flags |= Notification.FLAG_ONGOING_EVENT;
+						notificationManager.notify(1, notification);
+					}
 				}
 			}
 		}
